9594ed4adf60a259f6dcac899406e95d6a244042
Vsevolod Demidov <mvdemidov@gmail.com>
2021-12-02 00:47:27 +0300

    marathon

--- Marafon/js/main.js
+++ Marafon/js/main.js
@@ -1,12 +1,8 @@
 $(function(){
-
     $('.header-slider').slick({
         dots: true,
         vertical: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="./images/prev.svg" alt=""></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="./images/next.svg" alt=""></button>'
-
+        nextArrow: '<button type="button" class="slick-next"><img src="./images/next.svg" alt=""></button>',
     });
-
 });
-
