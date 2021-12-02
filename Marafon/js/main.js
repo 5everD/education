b6901ab61c0843243440c32daafac8cba5b100ae
Vsevolod Demidov <mvdemidov@gmail.com>
2021-12-02 22:36:52 +0300

    marathon day two

--- Marafon/js/main.js
+++ Marafon/js/main.js
@@ -6,3 +6,21 @@ $(function(){
         nextArrow: '<button type="button" class="slick-next"><img src="./images/next.svg" alt=""></button>',
     });
 });
+
+$('.product__name').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    focusOnSelect: true,
+    asNavFor: '.product__content',
+    vertical: true,
+    prevArrow: '<button type="button" class="product-prev"><img src="./images/prod-prev.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="product-next"><img src="./images/prod-next.svg" alt=""></button>',
+});
+
+$('.product__content').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.product__name',
+    arrows: false,
+    // fade: true
+});
