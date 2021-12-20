73e9d796583560844fa7d8eb3abbe2337fed7707
Vsevolod Demidov <mvdemidov@gmail.com>
2021-12-21 02:47:08 +0300

    project

--- React/unit_project/react_project/src/App.js
+++ React/unit_project/react_project/src/App.js
@@ -1,11 +1,11 @@
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
-import Create from "./components/Create";
-import Main from "./components/Main";
-import About from "./components/About";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Note from "./components/Note";
-// import Error from "./components/Error";
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Create from './components/Create';
+import Main from './components/Main';
+import About from './components/About';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Note from './components/Note';
+// import Error from './components/Error';
 
 
 function App() {
