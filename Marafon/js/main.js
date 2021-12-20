b12d534fce12ed9598e7e1d25c0e672eb816ec74
Vsevolod Demidov <mvdemidov@gmail.com>
2021-12-21 01:43:21 +0300

    project

--- Marafon/js/main.js
+++ Marafon/js/main.js
@@ -1,26 +1,53 @@
-$(function(){
+$(function() {
     $('.header-slider').slick({
         dots: true,
-        vertical: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="./images/prev.svg" alt=""></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="./images/next.svg" alt=""></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="../images/prev.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../images/next.svg" alt=""></button>',
+        fade: true,
+        responsive: [{
+            breakpoint: 361,
+            settings: {
+                dots: false,
+                arrows: false,
+                autoplay: true,
+                autoplaySpeed: 2000
+            }
+        }]
     });
-});
 
-$('.product__name').slick({
-    slidesToShow: 6,
-    slidesToScroll: 1,
-    focusOnSelect: true,
-    asNavFor: '.product__content',
-    vertical: true,
-    prevArrow: '<button type="button" class="product-prev"><img src="./images/prod-prev.svg" alt=""></button>',
-    nextArrow: '<button type="button" class="product-next"><img src="./images/prod-next.svg" alt=""></button>',
-});
+    $('.product__name').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        focusOnSelect: true,
+        asNavFor: '.product__content',
+        vertical: true,
+        prevArrow: '<button type="button" class="product-prev"><img src="../images/prod-prev.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="product-next"><img src="../images/prod-next.svg" alt=""></button>',
+        responsive: [{
+            breakpoint: 891,
+            settings: {
+                vertical: false,
+                slidesToShow: 3,
+                arrows: false,
+                dots: true,
+                breakpoint: 461,
+                settings: {
+                    slidesToShow: 1,
+                    arrows: false,
+                    dots: true,
+                }
+            }
+        }]
+    });
 
-$('.product__content').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    asNavFor: '.product__name',
-    arrows: false,
-    // fade: true
+    $('.product__content').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.product__name',
+        arrows: false,
+    });
+
+    $('.menu__btn').on('click', function () {
+        $('.menu__list').toggleClass('menu__list--active');
+    });
 });
