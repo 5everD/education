687fe3fff8d02639f83b9a9b6e45fe8c0d8fbb5d
Vsevolod Demidov <mvdemidov@gmail.com>
2021-12-23 04:16:11 +0300

    my project

--- React/unit_project/react_project/src/components/Header.js
+++ React/unit_project/react_project/src/components/Header.js
@@ -1,36 +1,50 @@
 import {NavLink} from "react-router-dom";
-import {useEffect, useRef} from "react";
+import {useEffect, useRef, useState} from "react";
 
 function Header() {
-    const componentRef  = useRef(null);
+    const componentRef = useRef(null);
+    const [refresh, setRefresh] = useState(false)
+    const [load, setload] = useState(false)
 
+
+    function go() {
+        setRefresh(true)
+    }
     useEffect(()=>{
-        const lis = componentRef.current.querySelectorAll('.list');
-        // const nlink = componentRef.current.querySelectorAll('.navlink');
-        function activeLink(){
-            lis.forEach((item)=>
-                item.classList.remove('active'));
-            this.classList.add('active')
-        }
-        lis.forEach((item)=>
-        item.addEventListener('click', activeLink))
+
     },[])
 
+    useEffect(() => {
+        const lis = componentRef.current.querySelectorAll('.list');
+        const nlink = componentRef.current.querySelectorAll('.navlink');
+        function activeLink() {
+            for (let i = 0; i < nlink.length; i++) {
+                if (nlink[i].classList.contains('active')) {
+                    for (let k = 0; k < lis.length; k++) {
+                        lis[k].classList.remove('active')
+                    }
+                    lis[i].classList.add('active')
+                }
+            }
+        }
+        activeLink()
+        setRefresh(false)
+    }, [refresh])
 
 
     return (
         <div ref={componentRef}>
             <nav className="navigation">
                 <ul>
-                    <li className="list">
-                        <NavLink exact to="/"  className="navlink">
+                    <li className="list" onClick={go}>
+                        <NavLink exact to="/" className="navlink">
                             <span className="icon">
                                 <ion-icon name="home-outline"/>
                             </span>
                             <span className="text">Home</span>
                         </NavLink>
                     </li>
-                    <li className="list">
+                    <li className="list" onClick={go}>
                         <NavLink exact to="/create" className="navlink">
                             <span className="icon">
                                 <ion-icon name="code-download-outline"/>
@@ -38,7 +52,7 @@ function Header() {
                             <span className="text">Create</span>
                         </NavLink>
                     </li>
-                    <li className="list">
+                    <li className="list" onClick={go}>
                         <NavLink exact to="/note" className="navlink">
                             <span className="icon">
                                 <ion-icon name="document-text-outline"/>
@@ -46,7 +60,7 @@ function Header() {
                             <span className="text">Note</span>
                         </NavLink>
                     </li>
-                    <li className="list">
+                    <li className="list" onClick={go}>
                         <NavLink exact to="/about" className="navlink">
                             <span className="icon">
                                 <ion-icon name="information-circle-outline"/>
--- React/unit_project/react_project/src/components/Main.js
+++ React/unit_project/react_project/src/components/Main.js
@@ -1,13 +1,15 @@
+import {NavLink} from "react-router-dom";
+
 function Main() {
 
     return (
         <div className="container">
             <div className="main">
                 <div className="note__create">
-                    <a href="/create">Создать Note</a>
+                    <NavLink exact to="/create">Создать Note</NavLink>
                 </div>
                 <div className="note__view">
-                    <a href="/note">Посмотреть Note</a>
+                    <NavLink exact to="/note">Посмотреть Note</NavLink>
                 </div>
             </div>
         </div>
