abdb38719fa4dbc64f08251e98fb7e9441cec5c1
Demidov Vsevolod <demidov.vsevolod@uep24.ru>
2022-02-07 15:49:00 +0300

    'work'

---
+++ TS/Types.ts
@@ -0,0 +1,12 @@
+const isFetching: boolean = true
+const isLoading: boolean = false
+
+const int: number = 42
+const message: string = "42"
+const numberArray: number[] = [1, 1, 2, 3, 5, 8, 13]
+const numberArray2: Array<number> = [1, 1, 2, 3, 5, 8, 13]
+
+const contact: [string, number] = ['Vsevolod', 1234567]
+
+
+
